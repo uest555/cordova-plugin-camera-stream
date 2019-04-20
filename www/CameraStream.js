c0720f1cbf8f1503b85e8d8e8e4dd0725ddd494d
Andrea <uest555@gmail.com>
2019-04-20 19:23:45 +0200

    Update CameraStream.js
    Changes for avoiding plugin Name problem

--- www/CameraStream.js
+++ www/CameraStream.js
@@ -2,32 +2,19 @@ var exec = require('cordova/exec');
 
 // Camera: front or back
 exports.startCapture = function (camera) {
-    exec(null, null, 'CameraBaseStream', 'startCapture', [camera]);
+    //exec(null, null, 'CameraBaseStream', 'startCapture', [camera]);
+    exec(null, null, 'CameraStream', 'startCapture', [camera]);
 };
 
 exports.pause = function () {
-    exec(null, null, 'CameraBaseStream', 'pause', []);
+    //exec(null, null, 'CameraBaseStream', 'pause', []);
+    exec(null, null, 'CameraStream', 'pause', []);
 };
 
 exports.resume = function () {
-    exec(null, null, 'CameraBaseStream', 'resume', []);
+    //exec(null, null, 'CameraBaseStream', 'resume', []);
+    exec(null, null, 'CameraStream', 'resume', []);
 };
 
 exports.capture = function(data){
-    /**
-     * Just a placeholder, iOS is going to call this
-     * directly when the camera stream is ready.
-     * 
-     * Define this func in your app to use the data.
-     * 
-     * Example:
-     * 
-     * var imageData = document.getElementById('<imageId>')
-     * 
-     * cordova.plugins.CameraBase64.capture = function(data){
-     *      image.src = imageData
-     * }
-     * //Starting the stream
-     * cordova.plugins.CameraBase64.startCapture();
-     */
 };
