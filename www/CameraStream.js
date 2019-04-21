6922ec1f7dfc8fef29000e2390d480d379daa573
Andrea <uest555@gmail.com>
2019-04-21 16:09:15 +0200

    Update CameraStream.js

--- www/CameraStream.js
+++ www/CameraStream.js
@@ -2,17 +2,14 @@ var exec = require('cordova/exec');
 
 // Camera: front or back
 exports.startCapture = function (camera) {
-    //exec(null, null, 'CameraBaseStream', 'startCapture', [camera]);
     exec(null, null, 'CameraStream', 'startCapture', [camera]);
 };
 
 exports.pause = function () {
-    //exec(null, null, 'CameraBaseStream', 'pause', []);
     exec(null, null, 'CameraStream', 'pause', []);
 };
 
 exports.resume = function () {
-    //exec(null, null, 'CameraBaseStream', 'resume', []);
     exec(null, null, 'CameraStream', 'resume', []);
 };
 
